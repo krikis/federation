afef1e9a68d320c69dffc73330c79115dcbb40ee
Samuel Esposito <samuel.esposito@gmail.com>
2013-06-07 10:47:27 +0200

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Federation::Application.config.secret_token = '3897b9ec41b05c0d506c9acc8890ec4bb5acc387eca1b6403846183a32896dc58365cf21ba30cadba5c3e3cc7e207e7f05dbb99cad2ffaf912eadab4a1a69327'
