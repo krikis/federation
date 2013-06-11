e429b2cf118714f7151c4dee9ca7ea14c1dfd4fa
Samuel Esposito <samuel.esposito@gmail.com>
2013-06-11 11:33:09 +0200

    merge epd_patients with same id

--- db/migrate/20130607092634_create_epd_patients.rb
+++ db/migrate/20130607092634_create_epd_patients.rb
@@ -1,25 +1,22 @@
 class CreateEpdPatients < ActiveRecord::Migration
   def up
-    # add bigint to int conversion convenience function
-    execute 'create function big2int (n bigint) returns integer return n'
-
     # create the epd_patients view
     execute <<-SQL
       create view epd_patients as
       select r.epd_patient_id,
-             coalesce(m.date, u.date) as date,
-             coalesce(case m.gender when 'M' then 'Male' when 'F' then 'Female' end, u.gender) as gender,
-             coalesce(m.name, u.name) as name,
-             big2int(coalesce(m.phone_number, p.phone)) as home_phone_nr
+             min(coalesce(m.date, u.date)) as date,
+             min(coalesce(case m.gender when 'M' then 'Male' when 'F' then 'Female' end, u.gender)) as gender,
+             min(coalesce(m.name, u.name)) as name,
+             min(coalesce(m.phone_number, p.phone)) as home_phone_nr
       from epd_patient_plays_role_using_local_nrs r
       left join m_patients m on r.role = 'MZKH' and r.local_nr = m.m_patient_nr
       left join u_patients u on r.role = 'UMCG' and r.local_nr = u.u_patient_nr
       left join u_patient_has_phones p on p.u_patient_nr = u.u_patient_nr
+      group by r.epd_patient_id
     SQL
   end
 
   def down
-    execute 'drop function big2int'
     execute 'drop view epd_patients'
   end
 end
--- spec/models/epd_patient_spec.rb
+++ spec/models/epd_patient_spec.rb
@@ -2,27 +2,28 @@ require 'spec_helper'
 
 describe EpdPatient do
 
-  context 'when a MZKH role exists' do
-    let(:local_nr) { 110 }
-    let!(:role) do
+  context 'when an MZKH role exists' do
+    let(:epd_patient_id) { 110 }
+    let(:mzkh_local_nr) { 111 }
+    let!(:mzkh_role) do
       Fabricate :epd_patient_plays_role_using_local_nr,
                 role: 'MZKH',
-                local_nr: local_nr,
-                epd_patient_id: 111
+                local_nr: mzkh_local_nr,
+                epd_patient_id: epd_patient_id
     end
 
     context 'and an MPatient exists with the role local nr' do
       let!(:m_patient) do
-        Fabricate :m_patient, m_patient_nr: local_nr, date: Date.today,
-                              gender: 'M', name: 'some_name'
+        Fabricate :m_patient, m_patient_nr: mzkh_local_nr, date: Date.today,
+                              gender: 'M', name: 'some_name', phone_number: 87654321
       end
 
       it 'an EpdPatient exists with the role EpdPatientId' do
-        EpdPatient.first.epd_patient_id.should eq(role.epd_patient_id)
+        EpdPatient.first.epd_patient_id.should eq(epd_patient_id)
       end
 
       it 'an EpdPatient exists with the MPatient date' do
-        EpdPatient.first.date.should eq(m_patient.date)
+        EpdPatient.first.date.to_s.should eq(m_patient.date.to_s)
       end
 
       it 'an EpdPatient exists with the MPatient gender' do
@@ -40,23 +41,27 @@ describe EpdPatient do
   end
 
   context 'when a UMCG role exists' do
-    let(:local_nr) { 110 }
-    let!(:role) do
+    let(:epd_patient_id) { 110 }
+    let(:umcg_local_nr) { 112 }
+    let!(:umcg_role) do
       Fabricate :epd_patient_plays_role_using_local_nr,
                 role: 'UMCG',
-                local_nr: local_nr,
-                epd_patient_id: 111
+                local_nr: umcg_local_nr,
+                epd_patient_id: epd_patient_id
     end
 
     context 'and a UPatient exists with the role local nr' do
-      let!(:u_patient) { Fabricate :u_patient, u_patient_nr: local_nr }
+      let!(:u_patient) do
+        Fabricate :u_patient, u_patient_nr: umcg_local_nr, date: 1.day.ago,
+                              gender: 'Female', name: 'some_other_name'
+      end
 
       it 'an EpdPatient exists with the role EpdPatientId' do
-        EpdPatient.first.epd_patient_id.should eq(role.epd_patient_id)
+        EpdPatient.first.epd_patient_id.should eq(epd_patient_id)
       end
 
       it 'an EpdPatient exists with the UPatient date' do
-        EpdPatient.first.date.should eq(u_patient.date)
+        EpdPatient.first.date.to_s.should eq(u_patient.date.to_s)
       end
 
       it 'an EpdPatient exists with the UPatient gender' do
@@ -68,11 +73,47 @@ describe EpdPatient do
       end
 
       context 'and a UPatientHasPhone exists with the UPatient u_patient_nr' do
-        let!(:u_patient_phone) { UPatientHasPhone.create u_patient_nr: local_nr, phone: 12345678 }
+        let!(:u_patient_phone) { UPatientHasPhone.create u_patient_nr: umcg_local_nr, phone: 12345678 }
 
         it 'an EpdPatient exists with the UPatient phone_number' do
           EpdPatient.first.home_phone_nr.should eq(u_patient_phone.phone)
         end
+      end
+    end
+  end
+
+  context 'when both an MZKH and UMCG role exists' do
+    let(:epd_patient_id) { 110 }
+    let(:mzkh_local_nr) { 111 }
+    let!(:mzkh_role) do
+      Fabricate :epd_patient_plays_role_using_local_nr,
+                role: 'MZKH',
+                local_nr: mzkh_local_nr,
+                epd_patient_id: epd_patient_id
+    end
+    let(:umcg_local_nr) { 112 }
+    let!(:umcg_role) do
+      Fabricate :epd_patient_plays_role_using_local_nr,
+                role: 'UMCG',
+                local_nr: umcg_local_nr,
+                epd_patient_id: epd_patient_id
+    end
+
+    context 'and both an MPatient and UPatient exists' do
+      let!(:m_patient) do
+        Fabricate :m_patient, m_patient_nr: mzkh_local_nr, date: Date.today, name: 'some_name'
+      end
+      let!(:u_patient) do
+        Fabricate :u_patient, u_patient_nr: umcg_local_nr, gender: 'Female'
+      end
+      let!(:u_patient_phone) { UPatientHasPhone.create u_patient_nr: umcg_local_nr, phone: 12345678 }
+
+      it 'returns the coalescence of both patients' do
+        EpdPatient.first.attributes.should eq("epd_patient_id" => epd_patient_id,
+                                              "date" => m_patient.date.to_s,
+                                              "gender" => u_patient.gender,
+                                              "name" => m_patient.name,
+                                              "home_phone_nr" => u_patient_phone.phone)
       end
     end
   end
