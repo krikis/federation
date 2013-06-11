8ce6e172c21b0f20d6a28a3ce6afc8b17412be05
Samuel Esposito <samuel.esposito@gmail.com>
2013-06-11 15:03:37 +0200

    simplify specs

--- spec/models/epd_patient_spec.rb
+++ spec/models/epd_patient_spec.rb
@@ -3,13 +3,12 @@ require 'spec_helper'
 describe EpdPatient do
 
   context 'when an MZKH role exists' do
-    let(:epd_patient_id) { 110 }
     let(:mzkh_local_nr) { 111 }
     let!(:mzkh_role) do
       Fabricate :epd_patient_plays_role_using_local_nr,
                 role: 'MZKH',
                 local_nr: mzkh_local_nr,
-                epd_patient_id: epd_patient_id
+                epd_patient_id: 110
     end
 
     context 'and an MPatient exists with the role local nr' do
@@ -18,64 +17,43 @@ describe EpdPatient do
                               gender: 'M', name: 'some_name', phone_number: 87654321
       end
 
-      it 'has an instance with the role EpdPatientId' do
-        EpdPatient.first.epd_patient_id.should eq(epd_patient_id)
-      end
-
-      it 'has an instance with the MPatient date' do
-        EpdPatient.first.date.to_s.should eq(m_patient.date.to_s)
-      end
-
-      it 'has an instance with the MPatient gender' do
-        EpdPatient.first.gender.should eq('Male')
-      end
-
-      it 'has an instance with the MPatient name' do
-        EpdPatient.first.name.should eq(m_patient.name)
-      end
-
-      it 'has an instance with the MPatient phone_number' do
-        EpdPatient.first.home_phone_nr.should eq(m_patient.phone_number)
+      it 'has an instance containing this object attributes' do
+        EpdPatient.first.attributes.should eq("epd_patient_id" => mzkh_role.epd_patient_id,
+                                              "date" => m_patient.date,
+                                              "gender" => 'Male',
+                                              "name" => m_patient.name,
+                                              "home_phone_nr" => m_patient.phone_number)
       end
     end
   end
 
   context 'when a UMCG role exists' do
-    let(:epd_patient_id) { 110 }
     let(:umcg_local_nr) { 112 }
     let!(:umcg_role) do
       Fabricate :epd_patient_plays_role_using_local_nr,
                 role: 'UMCG',
                 local_nr: umcg_local_nr,
-                epd_patient_id: epd_patient_id
+                epd_patient_id: 110
     end
 
     context 'and a UPatient exists with the role local nr' do
       let!(:u_patient) do
-        Fabricate :u_patient, u_patient_nr: umcg_local_nr, date: 1.day.ago,
+        Fabricate :u_patient, u_patient_nr: umcg_local_nr, date: 1.day.ago.to_date,
                               gender: 'Female', name: 'some_other_name'
       end
 
-      it 'has an instance with the role EpdPatientId' do
-        EpdPatient.first.epd_patient_id.should eq(epd_patient_id)
-      end
-
-      it 'has an instance with the UPatient date' do
-        EpdPatient.first.date.to_s.should eq(u_patient.date.to_s)
-      end
-
-      it 'has an instance with the UPatient gender' do
-        EpdPatient.first.gender.should eq(u_patient.gender)
-      end
-
-      it 'has an instance with the UPatient name' do
-        EpdPatient.first.name.should eq(u_patient.name)
+      it 'has an instance containing this object attributes' do
+        EpdPatient.first.attributes.should eq("epd_patient_id" => umcg_role.epd_patient_id,
+                                              "date" => u_patient.date,
+                                              "gender" => u_patient.gender,
+                                              "name" => u_patient.name,
+                                              "home_phone_nr" => nil)
       end
 
       context 'and a UPatientHasPhone exists with the UPatient u_patient_nr' do
         let!(:u_patient_phone) { UPatientHasPhone.create u_patient_nr: umcg_local_nr, phone: 12345678 }
 
-        it 'has an instance with the UPatient phone_number' do
+        it 'has an instance containing this UPatient phone_number' do
           EpdPatient.first.home_phone_nr.should eq(u_patient_phone.phone)
         end
       end
@@ -110,7 +88,7 @@ describe EpdPatient do
 
       it 'has an instance which is the coalescence of both patients' do
         EpdPatient.first.attributes.should eq("epd_patient_id" => epd_patient_id,
-                                              "date" => m_patient.date.to_s,
+                                              "date" => m_patient.date,
                                               "gender" => u_patient.gender,
                                               "name" => m_patient.name,
                                               "home_phone_nr" => u_patient_phone.phone)
