c4419a96aa2c2937ab90cf9b1c53235fcddc79ef
Samuel Esposito <samuel.esposito@gmail.com>
2013-06-12 10:39:14 +0200

    fetch objects from db

--- app/controllers/federations_controller.rb
+++ app/controllers/federations_controller.rb
@@ -1,2 +1,10 @@
 class FederationsController < ApplicationController
+
+  def show
+    @m_patients = MPatient.all
+    @u_patients = UPatient.all
+    @gis = EpdPatientPlaysRoleUsingLocalNr.all
+    @epd_patients = EpdPatient.all
+  end
+
 end
