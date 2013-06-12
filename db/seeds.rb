73684acf87a564816434bea065540ecabb2f0f04
Samuel Esposito <samuel.esposito@gmail.com>
2013-06-13 00:33:53 +0200

    more randomness

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,52 +7,66 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+def random_birthdate
+  (random_phone_nr + rand(random_phone_nr)).days.ago
+end
+
+def random_date
+  rand(random_phone_nr).days.ago
+end
+
+def random_phone_nr
+  (SecureRandom.random_number * 100000000).round
+end
+
 # ------------------------------------ MPatients -------------------------------
 m_patient_1 = 123
-MPatient.create m_patient_nr: m_patient_1, date: (5000 + rand(10000)).days.ago, gender: 'M', name: 'Jan met de Pet', phone_number: 87654321
+MPatient.create m_patient_nr: m_patient_1, date: random_birthdate, gender: 'M', name: 'Jan met de Pet', phone_number: random_phone_nr
 MPatientIsAllergicToDrug.create drug_code: 'Drug1', m_patient_nr: m_patient_1
 MPatientIsAllergicToDrug.create drug_code: 'Drug2', m_patient_nr: m_patient_1
-MPatientTreatmentInvolvesDrug.create date: rand(2000).days.ago, drug_code: 'Drug3', m_patient_nr: m_patient_1, specialist_id: 10
-MPatientTreatmentInvolvesDrug.create date: rand(2000).days.ago, drug_code: 'Drug4', m_patient_nr: m_patient_1, specialist_id: 11
-MPatientAdmittedForAdmissionReasonOnDate.create admission_reason_code: 'AdmissionReason1', date: rand(2000).days.ago, m_patient_nr: m_patient_1
-MPatientAdmittedForAdmissionReasonOnDate.create admission_reason_code: 'AdmissionReason2', date: rand(2000).days.ago, m_patient_nr: m_patient_1
+MPatientTreatmentInvolvesDrug.create date: random_date, drug_code: 'Drug3', m_patient_nr: m_patient_1, specialist_id: 10
+MPatientTreatmentInvolvesDrug.create date: random_date, drug_code: 'Drug4', m_patient_nr: m_patient_1, specialist_id: 11
+MPatientAdmittedForAdmissionReasonOnDate.create admission_reason_code: 'AdmissionReason1', date: random_date, m_patient_nr: m_patient_1
+MPatientAdmittedForAdmissionReasonOnDate.create admission_reason_code: 'AdmissionReason2', date: random_date, m_patient_nr: m_patient_1
 
 m_patient_2 = 124
-MPatient.create m_patient_nr: m_patient_2, date: (5000 + rand(10000)).days.ago, gender: 'F', name: 'T. Ester', phone_number: 12345678
-MartiniTreatmentInvolvesDrug.create date: rand(2000).days.ago, drug_code: 'Drug1', martini_patient_nr: m_patient_2, martini_doctor_id: 10
-MPatientAdmittedForAdmissionReasonOnDate.create admission_reason_code: 'AdmissionReason1', date: rand(2000).days.ago, m_patient_nr: m_patient_2
+MPatient.create m_patient_nr: m_patient_2, date: random_birthdate, gender: 'F', name: 'T. Ester', phone_number: random_phone_nr
+MartiniTreatmentInvolvesDrug.create date: random_date, drug_code: 'Drug1', martini_patient_nr: m_patient_2, martini_doctor_id: 10
+MPatientAdmittedForAdmissionReasonOnDate.create admission_reason_code: 'AdmissionReason1', date: random_date, m_patient_nr: m_patient_2
 
 m_patient_3 = 125
-MPatient.create m_patient_nr: m_patient_3, date: (5000 + rand(10000)).days.ago, name: 'Second Opinion'
-MartiniTreatmentInvolvesDrug.create date: rand(2000).days.ago, drug_code: 'Drug1', martini_patient_nr: m_patient_3, martini_doctor_id: 10
-MPatientAdmittedForAdmissionReasonOnDate.create admission_reason_code: 'AdmissionReason2', date: rand(2000).days.ago, m_patient_nr: m_patient_3
+MPatient.create m_patient_nr: m_patient_3, date: random_birthdate, name: 'Second Opinion'
+MartiniTreatmentInvolvesDrug.create date: random_date, drug_code: 'Drug1', martini_patient_nr: m_patient_3, martini_doctor_id: 10
+MPatientAdmittedForAdmissionReasonOnDate.create admission_reason_code: 'AdmissionReason2', date: random_date, m_patient_nr: m_patient_3
 
 # ------------------------------------ UPatients -------------------------------
 u_patient_1 = 2030
-UPatient.create u_patient_nr: u_patient_1, date: (5000 + rand(10000)).days.ago, gender: 'Male', name: 'P. Atiënt'
-UPatientHasPhone.create phone: 1234567, u_patient_nr: u_patient_1
-UPatientHasPhone.create phone: 123456, u_patient_nr: u_patient_1
-UPatientHasPhone.create phone: 12345678, u_patient_nr: u_patient_1
-UPatientTreatment.create date: rand(2000).days.ago, u_patient_nr: u_patient_1, doctor_id: 12
-UPatientTreatmentInvolvesDrug.create date: rand(2000).days.ago, drug_code: 'Drug5', u_patient_nr: u_patient_1
-UPatientTreatmentInvolvesDrug.create date: rand(2000).days.ago, drug_code: 'Drug6', u_patient_nr: u_patient_1
-UPatientWasAdmittedForAdmittanceReasonOnDate.create admittance_reason: 'AdmittanceReason1', date: rand(2000).days.ago, u_patient_nr: u_patient_1
+UPatient.create u_patient_nr: u_patient_1, date: random_birthdate, gender: 'Male', name: 'P. Atiënt'
+UPatientHasPhone.create phone: random_phone_nr, u_patient_nr: u_patient_1
+UPatientHasPhone.create phone: random_phone_nr, u_patient_nr: u_patient_1
+UPatientHasPhone.create phone: random_phone_nr, u_patient_nr: u_patient_1
+UPatientTreatment.create date: random_date, u_patient_nr: u_patient_1, doctor_id: 12
+UPatientTreatmentInvolvesDrug.create date: random_date, drug_code: 'Drug5', u_patient_nr: u_patient_1
+UPatientTreatmentInvolvesDrug.create date: random_date, drug_code: 'Drug6', u_patient_nr: u_patient_1
+UPatientWasAdmittedForAdmittanceReasonOnDate.create admittance_reason: 'AdmittanceReason1', date: random_date, u_patient_nr: u_patient_1
 
 u_patient_2 = 2031
-UPatient.create u_patient_nr: u_patient_2, date: rand(2000).days.ago, gender: 'Female', name: 'Janette Fictief'
-UPatientHasPhone.create phone: 123456789, u_patient_nr: u_patient_2
-UPatientHasPhone.create phone: 12345678910, u_patient_nr: u_patient_2
-UPatientTreatment.create date: rand(2000).days.ago, u_patient_nr: u_patient_2, doctor_id: 13
-UPatientTreatmentInvolvesDrug.create date: rand(2000).days.ago, drug_code: 'Drug3', u_patient_nr: u_patient_2
-UPatientTreatmentInvolvesDrug.create date: rand(2000).days.ago, drug_code: 'Drug7', u_patient_nr: u_patient_2
-UPatientWasAdmittedForAdmittanceReasonOnDate.create admittance_reason: 'AdmittanceReason2', date: rand(2000).days.ago, u_patient_nr: u_patient_2
+UPatient.create u_patient_nr: u_patient_2, date: random_date, gender: 'Female', name: 'Janette Fictief'
+UPatientHasPhone.create phone: random_phone_nr, u_patient_nr: u_patient_2
+UPatientHasPhone.create phone: random_phone_nr, u_patient_nr: u_patient_2
+UPatientHasPhone.create phone: random_phone_nr, u_patient_nr: u_patient_2
+UPatientTreatment.create date: random_date, u_patient_nr: u_patient_2, doctor_id: 13
+UPatientTreatmentInvolvesDrug.create date: random_date, drug_code: 'Drug3', u_patient_nr: u_patient_2
+UPatientTreatmentInvolvesDrug.create date: random_date, drug_code: 'Drug7', u_patient_nr: u_patient_2
+UPatientWasAdmittedForAdmittanceReasonOnDate.create admittance_reason: 'AdmittanceReason2', date: random_date, u_patient_nr: u_patient_2
 
 u_patient_3 = 2032
 UPatient.create u_patient_nr: u_patient_3, gender: 'Female'
-UPatientHasPhone.create phone: 123456789, u_patient_nr: u_patient_3
-UPatientHasPhone.create phone: 12345678, u_patient_nr: u_patient_3
-UPatientTreatmentInvolvesDrug.create date: rand(2000).days.ago, drug_code: 'Drug8', u_patient_nr: u_patient_3
-UPatientWasAdmittedForAdmittanceReasonOnDate.create admittance_reason: 'AdmittanceReason3', date: rand(2000).days.ago, u_patient_nr: u_patient_3
+UPatientHasPhone.create phone: random_phone_nr, u_patient_nr: u_patient_3
+UPatientHasPhone.create phone: random_phone_nr, u_patient_nr: u_patient_3
+UPatientHasPhone.create phone: random_phone_nr, u_patient_nr: u_patient_3
+UPatientTreatmentInvolvesDrug.create date: random_date, drug_code: 'Drug8', u_patient_nr: u_patient_3
+UPatientWasAdmittedForAdmittanceReasonOnDate.create admittance_reason: 'AdmittanceReason3', date: random_date, u_patient_nr: u_patient_3
 
 # ------------------------------------ Gis -------------------------------------
 
