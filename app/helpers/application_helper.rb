859377ff2667ee1dfce0a97779b950054d066b98
Samuel Esposito <samuel.esposito@gmail.com>
2013-06-12 21:19:53 +0200

    add generic views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,15 @@ module ApplicationHelper
     content_tag(:i, "", :class => "icon-#{name} icon-#{color}")
   end
 
+  def form_text_element(label_text, control_text = "", &block)
+    content_tag(:div, class: "control-group text") do
+      content_tag(:label, label_text, class: "control-label") +
+      content_tag(:div, class: "controls") do
+        output = block.call if block_given?
+        control_text.to_s << output if output.is_a? String
+        control_text
+      end
+    end
+  end
+
 end
