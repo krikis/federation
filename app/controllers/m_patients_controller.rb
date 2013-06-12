b7feaef1cfe1f39540a651faae303c5aceb83b07
Samuel Esposito <samuel.esposito@gmail.com>
2013-06-12 17:19:35 +0200

    fix updating dates

--- app/controllers/m_patients_controller.rb
+++ app/controllers/m_patients_controller.rb
@@ -5,7 +5,10 @@ class MPatientsController < ApplicationController
   end
 
   def update
-
+    if m_patient = MPatient.find_by_id(params[:id])
+      m_patient.update_attributes params[:m_patient]
+    end
+    redirect_to root_path
   end
 
   def destroy
