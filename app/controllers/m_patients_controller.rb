ca5f8e8966c03b27e679d82ca322ab7c2e23592b
Samuel Esposito <samuel.esposito@gmail.com>
2013-06-12 17:29:40 +0200

    create and update admissions

--- app/controllers/m_patients_controller.rb
+++ app/controllers/m_patients_controller.rb
@@ -7,6 +7,15 @@ class MPatientsController < ApplicationController
   def update
     if m_patient = MPatient.find_by_id(params[:id])
       m_patient.update_attributes params[:m_patient]
+      admissions = params[:m_patient_admitted_for_admission_reason_on_date]
+      new_admissions = admissions.delete(:new)
+      admissions.keys.each do |admission_id|
+        MPatientAdmittedForAdmissionReasonOnDate.find_by_id(admission_id).andand
+          .update_attributes admissions[admission_id]
+      end
+      new_admissions.andand.each do |new_admission|
+        MPatientAdmittedForAdmissionReasonOnDate.create new_admission
+      end
     end
     redirect_to root_path
   end
