f4a5a3fc2543100227881c47b15935f73bfcf520
Samuel Esposito <samuel.esposito@gmail.com>
2013-06-12 13:39:55 +0200

    implement destroy

--- app/controllers/m_patients_controller.rb
+++ app/controllers/m_patients_controller.rb
@@ -1,2 +1,15 @@
 class MPatientsController < ApplicationController
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    MPatient.find_by_id(params[:id]).andand.destroy
+  end
+
 end
