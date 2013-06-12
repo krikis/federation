34c5c4367eed682fa79a63b80177971a35a26eba
Samuel Esposito <samuel.esposito@gmail.com>
2013-06-12 20:13:16 +0200

    create new m_patient

--- app/controllers/m_patients_controller.rb
+++ app/controllers/m_patients_controller.rb
@@ -1,20 +1,30 @@
 class MPatientsController < ApplicationController
 
   def create
-
+    if m_patient_params = params[:m_patient]
+      MPatient.create m_patient_params
+      if admissions = params[:m_patient_admitted_for_admission_reason_on_date]
+        new_admissions = admissions.delete(:new)
+        new_admissions.andand.each do |new_admission|
+          MPatientAdmittedForAdmissionReasonOnDate.create new_admission
+        end
+      end
+    end
+    redirect_to root_path
   end
 
   def update
     if m_patient = MPatient.find_by_id(params[:id])
       m_patient.update_attributes params[:m_patient]
-      admissions = params[:m_patient_admitted_for_admission_reason_on_date]
-      new_admissions = admissions.delete(:new)
-      admissions.keys.each do |admission_id|
-        MPatientAdmittedForAdmissionReasonOnDate.find_by_id(admission_id).andand
-          .update_attributes admissions[admission_id]
-      end
-      new_admissions.andand.each do |new_admission|
-        MPatientAdmittedForAdmissionReasonOnDate.create new_admission
+      if admissions = params[:m_patient_admitted_for_admission_reason_on_date]
+        new_admissions = admissions.delete(:new)
+        admissions.keys.each do |admission_id|
+          MPatientAdmittedForAdmissionReasonOnDate.find_by_id(admission_id).andand
+            .update_attributes admissions[admission_id]
+        end
+        new_admissions.andand.each do |new_admission|
+          MPatientAdmittedForAdmissionReasonOnDate.create new_admission
+        end
       end
     end
     redirect_to root_path
