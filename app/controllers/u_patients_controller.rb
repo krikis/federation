2bcb3024824399b57856f121aba0810cc6d3f909
Samuel Esposito <samuel.esposito@gmail.com>
2013-06-13 22:21:42 +0200

    implement UPatient controller

--- app/controllers/u_patients_controller.rb
+++ app/controllers/u_patients_controller.rb
@@ -1,2 +1,39 @@
 class UPatientsController < ApplicationController
+
+  def create
+    if u_patient_params = params[:u_patient]
+      UPatient.create u_patient_params
+      UPatient.associations.each do |association|
+        params[association.name].andand[:new].andand.each do |new_associated|
+          association.klass.create new_associated
+        end
+      end
+    end
+    redirect_to root_path
+  end
+
+  def update
+    if u_patient = UPatient.find_by_id(params[:id])
+      u_patient.update_attributes params[:u_patient] if params[:u_patient]
+      UPatient.associations.each do |association|
+        if associated = params[association.name]
+          new_associated = associated.delete(:new)
+          associated.keys.each do |associated_id|
+            association.klass.find_by_id(associated_id).andand
+              .update_attributes associated[associated_id]
+          end
+          new_associated.andand.each do |new_associated|
+            association.klass.create new_associated
+          end
+        end
+      end
+    end
+    redirect_to root_path
+  end
+
+  def destroy
+    UPatient.find_by_id(params[:id]).andand.destroy
+    redirect_to root_path
+  end
+
 end
