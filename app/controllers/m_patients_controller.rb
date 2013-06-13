416c15ac1c72aa4907b1ba178fd78ec1e47762c5
Samuel Esposito <samuel.esposito@gmail.com>
2013-06-13 22:20:06 +0200

    finalize MPatient controller

--- app/controllers/m_patients_controller.rb
+++ app/controllers/m_patients_controller.rb
@@ -3,10 +3,9 @@ class MPatientsController < ApplicationController
   def create
     if m_patient_params = params[:m_patient]
       MPatient.create m_patient_params
-      if admissions = params[:m_patient_admitted_for_admission_reason_on_date]
-        new_admissions = admissions.delete(:new)
-        new_admissions.andand.each do |new_admission|
-          MPatientAdmittedForAdmissionReasonOnDate.create new_admission
+      MPatient.associations.each do |association|
+        params[association.name].andand[:new].andand.each do |new_associated|
+          association.klass.create new_associated
         end
       end
     end
@@ -16,14 +15,16 @@ class MPatientsController < ApplicationController
   def update
     if m_patient = MPatient.find_by_id(params[:id])
       m_patient.update_attributes params[:m_patient] if params[:m_patient]
-      if admissions = params[:m_patient_admitted_for_admission_reason_on_date]
-        new_admissions = admissions.delete(:new)
-        admissions.keys.each do |admission_id|
-          MPatientAdmittedForAdmissionReasonOnDate.find_by_id(admission_id).andand
-            .update_attributes admissions[admission_id]
-        end
-        new_admissions.andand.each do |new_admission|
-          MPatientAdmittedForAdmissionReasonOnDate.create new_admission
+      MPatient.associations.each do |association|
+        if associated = params[association.name]
+          new_associated = associated.delete(:new)
+          associated.keys.each do |associated_id|
+            association.klass.find_by_id(associated_id).andand
+              .update_attributes associated[associated_id]
+          end
+          new_associated.andand.each do |new_associated|
+            association.klass.create new_associated
+          end
         end
       end
     end
