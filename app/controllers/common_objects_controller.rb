4c8390b38b5db51bc6207660dc99b60a1e8db45d
Samuel Esposito <samuel.esposito@gmail.com>
2013-06-16 14:30:28 +0200

    reject common_objects in m_patients and u_patients

---
+++ app/controllers/common_objects_controller.rb
@@ -0,0 +1,2 @@
+class CommonObjectsController < ApplicationController
+end
--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -1,10 +1,20 @@
 class ExchangesController < ApplicationController
 
   def show
-    @m_patients = MPatient.all
-    @u_patients = UPatient.all
-    @gis = Gi.all
     @common_objects = CommonObject.all
+    common_martini_patient_nrs = @common_objects.map do |common_object|
+      common_object.martini_patient.martini_patient_nr
+    end
+    @m_patients = MPatient.all.reject do |m_patient|
+      common_martini_patient_nrs.include? m_patient.m_patient_nr
+    end
+    common_umcg_patient_nrs = @common_objects.map do |common_object|
+      common_object.umcg_patient.umcg_patient_nr
+    end
+    @u_patients = UPatient.all.reject do |u_patient|
+      common_umcg_patient_nrs.include? u_patient.u_patient_nr
+    end
+    @gis = Gi.all
   end
 
 end
