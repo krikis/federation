df6fd1fd80f3477097def6d3dc93512e3e90e1b5
Samuel Esposito <samuel.esposito@gmail.com>
2013-06-16 15:13:26 +0200

    fix associations

--- app/models/u_patient_treatment.rb
+++ app/models/u_patient_treatment.rb
@@ -5,7 +5,7 @@ class UPatientTreatment < ActiveRecord::Base
   has_many :u_patient_treatment_involves_drugs,
      primary_key: 'u_patient_nr',
      foreign_key: 'u_patient_nr',
-     conditions: Proc.new { {date: 'date'} },
+     conditions: Proc.new { {date: date} },
      dependent: :destroy
 
 end
--- app/models/umcg_treatment.rb
+++ app/models/umcg_treatment.rb
@@ -5,7 +5,7 @@ class UmcgTreatment < ActiveRecord::Base
   has_many :umcg_treatment_involves_drugs,
            primary_key: 'umcg_patient_nr',
            foreign_key: 'umcg_patient_nr',
-           conditions: Proc.new { {date: 'date'} },
+           conditions: Proc.new { {date: date} },
            dependent: :destroy
 
 end
