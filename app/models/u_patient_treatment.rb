0f41de1357f33fc806418080e94ca124c08151ef
Samuel Esposito <samuel.esposito@gmail.com>
2013-06-16 23:20:02 +0200

    cleanup

--- app/models/u_patient_treatment.rb
+++ app/models/u_patient_treatment.rb
@@ -3,9 +3,8 @@ class UPatientTreatment < ActiveRecord::Base
   include HasDetails
 
   has_many :u_patient_treatment_involves_drugs,
-     primary_key: 'u_patient_nr',
-     foreign_key: 'u_patient_nr',
-     conditions: Proc.new { {date: date} },
-     dependent: :destroy
+           primary_key: 'u_patient_nr',
+           foreign_key: 'u_patient_nr',
+           dependent: :destroy
 
 end
