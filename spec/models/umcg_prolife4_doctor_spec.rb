de8cb9085136f850c3f53029bf68fd1246432387
Samuel Esposito <samuel.esposito@gmail.com>
2013-06-20 21:07:25 +0200

    more specs

--- spec/models/umcg_prolife4_doctor_spec.rb
+++ spec/models/umcg_prolife4_doctor_spec.rb
@@ -16,4 +16,31 @@ describe UmcgProlife4Doctor do
     UmcgProlife4Doctor.first.attributes.should eq('umcg_doctor_id' => umcg_doctor_id)
   end
 
+  it 'does not contain instances for umcg_doctors that treated a umcg_patient
+      which was also treated in the martini using a different drug' do
+    umcg_doctor_id = 10
+    epd_patient_id = 110
+    umcg_local_nr = 112
+    umcg_role = Fabricate :gi, role: 'UMCG', local_nr: umcg_local_nr, epd_patient_id: epd_patient_id
+    u_treatment = Fabricate :u_patient_treatment, date: Date.today, u_patient_nr: umcg_local_nr, doctor_id: umcg_doctor_id
+    mzkh_local_nr = 2030
+    mzkh_role = Fabricate :gi, role: 'MZKH', local_nr: mzkh_local_nr, epd_patient_id: epd_patient_id
+    m_treatment = Fabricate :m_patient_treatment_involves_drug, date: Date.today, drug_code: 'other_drug',
+                            m_patient_nr: mzkh_local_nr, specialist_id: 12345
+    UmcgProlife4Doctor.count.should be_zero
+  end
+
+  it 'does not contain instances for umcg_doctors that treated umcg_patients other
+      than patients which were treated in the martini using the drug ProLife4' do
+    umcg_doctor_id = 10
+    umcg_local_nr = 112
+    umcg_role = Fabricate :gi, role: 'UMCG', local_nr: umcg_local_nr, epd_patient_id: 110
+    u_treatment = Fabricate :u_patient_treatment, date: Date.today, u_patient_nr: umcg_local_nr, doctor_id: umcg_doctor_id
+    mzkh_local_nr = 2030
+    mzkh_role = Fabricate :gi, role: 'MZKH', local_nr: mzkh_local_nr, epd_patient_id: 111
+    m_treatment = Fabricate :m_patient_treatment_involves_drug, date: Date.today, drug_code: 'ProLife4',
+                            m_patient_nr: mzkh_local_nr, specialist_id: 12345
+    UmcgProlife4Doctor.count.should be_zero
+  end
+
 end
