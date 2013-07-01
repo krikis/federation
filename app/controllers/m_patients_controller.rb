b7613a647570a9d34762c4795d0fcd7f92a04e44
Samuel Esposito <samuel.esposito@gmail.com>
2013-07-02 01:01:53 +0200

    add transactions

--- app/controllers/m_patients_controller.rb
+++ app/controllers/m_patients_controller.rb
@@ -2,10 +2,12 @@ class MPatientsController < ApplicationController
 
   def create
     if m_patient_params = params[:m_patient]
-      MPatient.create m_patient_params
-      MPatient.associations.each do |association|
-        params[association.name].andand[:new].andand.each do |new_associated|
-          association.klass.create new_associated
+      MPatient.transaction do
+        MPatient.create m_patient_params
+        MPatient.associations.each do |association|
+          params[association.name].andand[:new].andand.each do |new_associated|
+            association.klass.create new_associated
+          end
         end
       end
     end
@@ -14,16 +16,18 @@ class MPatientsController < ApplicationController
 
   def update
     if m_patient = MPatient.find_by_id(params[:id])
-      m_patient.update_attributes params[:m_patient] if params[:m_patient]
-      MPatient.associations.each do |association|
-        if associated = params[association.name]
-          new_associated = associated.delete(:new)
-          associated.keys.each do |associated_id|
-            association.klass.find_by_id(associated_id).andand
-              .update_attributes associated[associated_id]
-          end
-          new_associated.andand.each do |new_associated|
-            association.klass.create new_associated
+      MPatient.transaction do
+        m_patient.update_attributes params[:m_patient] if params[:m_patient]
+        MPatient.associations.each do |association|
+          if associated = params[association.name]
+            new_associated = associated.delete(:new)
+            associated.keys.each do |associated_id|
+              association.klass.find_by_id(associated_id).andand
+                .update_attributes associated[associated_id]
+            end
+            new_associated.andand.each do |new_associated|
+              association.klass.create new_associated
+            end
           end
         end
       end
--- app/controllers/u_patients_controller.rb
+++ app/controllers/u_patients_controller.rb
@@ -2,10 +2,12 @@ class UPatientsController < ApplicationController
 
   def create
     if u_patient_params = params[:u_patient]
-      UPatient.create u_patient_params
-      UPatient.associations.each do |association|
-        params[association.name].andand[:new].andand.each do |new_associated|
-          association.klass.create new_associated
+      UPatient.transaction do
+        UPatient.create u_patient_params
+        UPatient.associations.each do |association|
+          params[association.name].andand[:new].andand.each do |new_associated|
+            association.klass.create new_associated
+          end
         end
       end
     end
@@ -14,16 +16,18 @@ class UPatientsController < ApplicationController
 
   def update
     if u_patient = UPatient.find_by_id(params[:id])
-      u_patient.update_attributes params[:u_patient] if params[:u_patient]
-      UPatient.associations.each do |association|
-        if associated = params[association.name]
-          new_associated = associated.delete(:new)
-          associated.keys.each do |associated_id|
-            association.klass.find_by_id(associated_id).andand
-              .update_attributes associated[associated_id]
-          end
-          new_associated.andand.each do |new_associated|
-            association.klass.create new_associated
+      UPatient.transaction do
+        u_patient.update_attributes params[:u_patient] if params[:u_patient]
+        UPatient.associations.each do |association|
+          if associated = params[association.name]
+            new_associated = associated.delete(:new)
+            associated.keys.each do |associated_id|
+              association.klass.find_by_id(associated_id).andand
+                .update_attributes associated[associated_id]
+            end
+            new_associated.andand.each do |new_associated|
+              association.klass.create new_associated
+            end
           end
         end
       end
