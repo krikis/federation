e07e6bf0086caff65462e3f808d4439b5936e0ed
Samuel Esposito <samuel.esposito@gmail.com>
2013-07-01 22:49:57 +0200

    cleanup

--- app/models/u_patient_treatment.rb
+++ app/models/u_patient_treatment.rb
@@ -5,7 +5,6 @@ class UPatientTreatment < ActiveRecord::Base
   has_many :u_patient_treatment_involves_drugs,
            primary_key: 'u_patient_nr',
            foreign_key: 'u_patient_nr',
-           # conditions: 'u_patient_treatment_involves_drugs.date = u_patient_treatments.date',
            dependent: :destroy
 
 end
--- app/models/umcg_treatment.rb
+++ app/models/umcg_treatment.rb
@@ -5,7 +5,6 @@ class UmcgTreatment < ActiveRecord::Base
   has_many :umcg_treatment_involves_drugs,
            primary_key: 'umcg_patient_nr',
            foreign_key: 'umcg_patient_nr',
-           conditions: 'umcg_treatment_involves_drugs.date = umcg_treatments.date',
            dependent: :destroy
 
 end
