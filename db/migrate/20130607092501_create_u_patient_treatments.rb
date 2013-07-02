10f56ae83220966f4fc9de632c329c48bc505a82
Samuel Esposito <samuel.esposito@gmail.com>
2013-07-02 09:55:11 +0200

    default foreign key to current date

--- db/migrate/20130607092501_create_u_patient_treatments.rb
+++ db/migrate/20130607092501_create_u_patient_treatments.rb
@@ -8,6 +8,15 @@ class CreateUPatientTreatments < ActiveRecord::Migration
       t.timestamps
     end
 
+    execute <<-SQL
+      create trigger default_u_patient_treatment_date_to_today before insert on u_patient_treatments
+        for each row begin
+          if new.date is null then
+            set new.date = curdate();
+          end if;
+        end;
+    SQL
+
     execute 'alter table u_patient_treatments add unique index (u_patient_nr, date)'
     execute 'alter table u_patient_treatments add foreign key (u_patient_nr) references u_patients(u_patient_nr) on update cascade'
   end
--- db/migrate/20130607092502_create_u_patient_treatment_involves_drugs.rb
+++ db/migrate/20130607092502_create_u_patient_treatment_involves_drugs.rb
@@ -8,6 +8,15 @@ class CreateUPatientTreatmentInvolvesDrugs < ActiveRecord::Migration
       t.timestamps
     end
 
+    execute <<-SQL
+      create trigger default_u_patient_treatment_involves_drugs_date_to_today before insert on u_patient_treatment_involves_drugs
+        for each row begin
+          if new.date is null then
+            set new.date = curdate();
+          end if;
+        end;
+    SQL
+
     execute 'alter table u_patient_treatment_involves_drugs add unique index (u_patient_nr, drug_code, date)'
     execute 'alter table u_patient_treatment_involves_drugs add foreign key (u_patient_nr, date) references u_patient_treatments(u_patient_nr, date) on update cascade'
   end
